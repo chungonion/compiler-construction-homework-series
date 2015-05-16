e5a55792b870497941a693681183b013b540e39b
Twan Coenraad <twan@coenraad.at>
2015-05-16 14:28:01 +0200

    Added test on G0 grammar

---
+++ homework-series-1/question3/G0Test.java
@@ -0,0 +1,26 @@
+package question3;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class G0Test {
+    @Test
+    public void test() {
+        assertEquals("(e (e (f (g (g (a a)) [ (e (e (f (g (a i)))) + (f (g (a 1)))) ]))) + (f (g (g (a b)) . field)))",
+                parseStringToTree("a[i+1]+b.field"));
+        assertEquals("(e (e (e (f (g (g (a ( (e (f ( Type ) (f (g (a x))))) ))) . i))) - (f (g (a 10)))) + (f (g (a y))))",
+                parseStringToTree("((Type)x).i-10+y"));
+    }
+
+    public String parseStringToTree(String text) {
+        CharStream stream = new ANTLRInputStream(text);
+        Lexer lexer = new G0Lexer(stream);
+        TokenStream tokens = new CommonTokenStream(lexer);
+        G0Parser parser = new G0Parser(tokens);
+        ParseTree tree = parser.e();
+        return tree.toStringTree(parser);
+    }
+}

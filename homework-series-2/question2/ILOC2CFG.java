ee73a0f9b1c591b33dbaaf86bb2d5214ca9131df
Twan Coenraad <twan@coenraad.at>
2015-06-15 19:33:47 +0200

    Refactoring ILOC2CFG

--- homework-series-2/question2/ILOC2CFG.java
+++ homework-series-2/question2/ILOC2CFG.java
@@ -46,33 +46,24 @@ public class ILOC2CFG {
 
     public Graph convert(Program prog) {
         Map<String, Node> nodes = new HashMap<>();
-        Set<String> reachableNodes = new HashSet<>();
         Graph graph = new Graph();
 
         Node currentNode = null;
         for (Instr instruction : prog.getInstr()) {
-            String label;
             if (currentNode == null || instruction.hasLabel() && currentNode.getEdges().size() > 0) {
-                label = instruction.getLabel().getValue();
-                currentNode = ILOC2CFG.getOrCreate(label, nodes, graph);
+                currentNode = ILOC2CFG.getOrCreate(instruction.getLabel().getValue(), nodes, graph);
             }
 
             for (Op op : instruction) {
-                if (op.getClaz() == OpClaz.CONTROL) {
-                    reachableNodes.add(currentNode.getId());
-
-                    if (op.getOpCode() == OpCode.cbr) {
-                        for (Operand arg : op.getArgs()) {
-                            if (arg == op.getArgs().get(0)) { // skip register arg
-                                continue;
-                            }
-                            reachableNodes.add(arg.toString());
-                            currentNode.addEdge(ILOC2CFG.getOrCreate(arg.toString(), nodes, graph));
+                if (op.getOpCode() == OpCode.cbr) {
+                    for (Operand arg : op.getArgs()) {
+                        if (arg == op.getArgs().get(0)) { // skip register arg
+                            continue;
                         }
-                    } else {
-                        reachableNodes.add(op.getArgs().get(0).toString());
-                        currentNode.addEdge(ILOC2CFG.getOrCreate(op.getArgs().get(0).toString(), nodes, graph));
+                        currentNode.addEdge(ILOC2CFG.getOrCreate(arg.toString(), nodes, graph));
                     }
+                } else if (op.getOpCode() == OpCode.jumpI) {
+                    currentNode.addEdge(ILOC2CFG.getOrCreate(op.getArgs().get(0).toString(), nodes, graph));
                 }
             }
         }
@@ -80,29 +71,17 @@ public class ILOC2CFG {
         Map<String, Node> minimalNodes = new HashMap<>();
         Set<Node> visitedNodes = new HashSet<>();
         Graph minimalGraph = new Graph();
-        Node previousMinimalNode = null;
 
         for (Node node : graph.getNodes()) {
-            if (reachableNodes.contains(node.getId())) {
-                if (previousMinimalNode != null) {
-                    previousMinimalNode.addEdge(node);
-                }
+            Node minimalNode = getOrCreate(node.getId(), minimalNodes, minimalGraph);
+            visitedNodes.add(minimalNode);
 
-                Node minimalNode = getOrCreate(node.getId(), minimalNodes, minimalGraph);
-                visitedNodes.add(minimalNode);
+            for (Node edge : node.getEdges()) {
+                minimalNode.addEdge(getOrCreate(edge.getId(), minimalNodes, minimalGraph));
+            }
 
-                if (node.getEdges().size() > 0) {
-                    for (Node edge : node.getEdges()) {
-                        minimalNode.addEdge(getOrCreate(edge.getId(), minimalNodes, minimalGraph));
-                    }
-
-                    if (visitedNodes.containsAll(node.getEdges())) {
-                        break; // apparently, we are only looking back
-                    }
-
-                } else {
-                    previousMinimalNode = minimalNode;
-                }
+            if (visitedNodes.containsAll(node.getEdges())) {
+                break; // apparently, we are only looking back
             }
         }
 
